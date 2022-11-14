e71247ece900b4b93cdb71277b5eca7a6f69c911
Valdano Simão <valdanosimao@gmail.com>
2022-11-14 15:49:46 -0300

    configurando a view binding

--- app/src/main/java/br/com/gastoviagem/MainActivity.kt
+++ app/src/main/java/br/com/gastoviagem/MainActivity.kt
@@ -1,11 +1,20 @@
 package br.com.gastoviagem
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+      ActivityMainBinding é a classe que foi criada para mapear os elementos
+      Inflate estamos falando, pegue o layout e infle/expanda
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
